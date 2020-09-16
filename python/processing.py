819c47eb4c41d4dc7ed0c53179c50361754f43e7
MatBenfield <MatBenfield@users.noreply.github.com>
2020-09-16 11:23:11 +0100

    ok
    Signed-off-by: MatBenfield <MatBenfield@users.noreply.github.com>

--- python/processing.py
+++ python/processing.py
@@ -45,10 +45,10 @@ def get_hostname(url):
 
 # publish date
 def get_published_info(entry):
-    try: entry["published"]:
+    try: entry["published"]
         return entry["published"].split("T")[0]
     except: KeyError
-        try: entry["pubDate"]:
+        try: entry["pubDate"]
             return entry["pubDate"].split("T")[0]
         else:
             return "unknown"
